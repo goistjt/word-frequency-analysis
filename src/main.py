078a76731df8a3f7c2030420cc1326ebc82f3baf
Jeremiah Goist <jtgoist@gmail.com>
2021-08-03 21:01:38 -0400

    Added option to replace word suffixes with the appropriate value to analyze word count by root word

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from typing import List, Dict, Optional
 from functools import reduce
 from pprint import pprint
 
+ORDERED_INFLECTION_REPLACEMENTS = [("EZL", "R"), ("PZL", "AZ"), ("ZL", "A"), ("ZQ", ""), ("EVM", ""), ("LZ", ""), ("L", "")]
+
 
 def read_file(filepath: str) -> str:
     """
@@ -57,6 +59,25 @@ def remove_words_from_count(word_count: Dict[str, int], to_remove: List[str]) ->
         copy_count.pop(removal, None)
     return copy_count
 
+def normalize_word_tense(words: List[str]) -> List[str]:
+    """
+    Normalizes word tense by replacing the ends of inflected words with their original suffix
+    """
+    normalized_words = []
+    for word in words:
+        replaced=False
+        for inflection, replacement in ORDERED_INFLECTION_REPLACEMENTS:
+            if word.endswith(inflection):
+                new_word = word.rstrip(inflection) + replacement
+                normalized_words.append(new_word)
+                replaced = True
+                break
+        
+        if not replaced:
+            normalized_words.append(word)
+    
+    return normalized_words
+
 @click.group()
 def cli():
     pass
@@ -64,15 +85,18 @@ def cli():
 @click.command()
 @click.option("--filepath", help="Filepath to count words from", required=True)
 @click.option("--stopword-path", help="Filepath containing list of stopwords")
-def word_count(filepath: str, stopword_path: Optional[str]):
+@click.option("--word-stems", default=False, help="Whether or not different inflections count as the same word")
+def word_count(filepath: str, stopword_path: Optional[str], word_stems: bool = False):
     stopwords = []
     if stopword_path:
         stopwords = get_stopwords(stopword_path)
     file_contents = read_file(filepath)
     word_list = get_word_list(file_contents)
+    word_list = [word for word in word_list if word not in stopwords]
+    if word_stems:
+        word_list = normalize_word_tense(word_list)
     word_counts = get_word_counts(word_list)
-    word_count_without_stops = remove_words_from_count(word_counts, stopwords)
-    top_words = get_top_words(word_count_without_stops)
+    top_words = get_top_words(word_counts)
     click.echo(top_words)
 
 if __name__ == "__main__":
