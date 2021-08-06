4b5811f9a644b034669d5692163510ae7f698acb
Jeremiah Goist <jtgoist@gmail.com>
2021-08-05 20:18:34 -0400

    Implemented saving last 10 results to 10, and viewing the results

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
+from datetime import datetime
+import os
 import click
 import re
 from typing import List, Dict, Optional
-from functools import reduce
 from pprint import pprint
+import json
+import uuid
 
-ORDERED_INFLECTION_REPLACEMENTS = [("EZL", "R"), ("PZL", "AZ"), ("ZL", "A"), ("ZQ", ""), ("EVM", ""), ("LZ", ""), ("L", "")]
+
+ORDERED_INFLECTION_REPLACEMENTS = [
+    ("EZL", "R"),
+    ("PZL", "AZ"),
+    ("ZL", "A"),
+    ("ZQ", ""),
+    ("EVM", ""),
+    ("LZ", ""),
+    ("L", ""),
+]
 
 
 def read_file(filepath: str) -> str:
@@ -16,12 +28,14 @@ def read_file(filepath: str) -> str:
     file.close()
     return content
 
+
 def get_word_list(string: str) -> str:
     """
     Removes all non-alpha characters from the input and returns the list of resulting strings
     """
     non_alpha_pattern = r"[^a-zA-Z ]"
-    return re.sub(pattern=non_alpha_pattern, repl='', string=string).split()
+    return re.sub(pattern=non_alpha_pattern, repl="", string=string).split()
+
 
 def get_word_counts(word_list: List[str]) -> Dict[str, int]:
     """
@@ -33,15 +47,14 @@ def get_word_counts(word_list: List[str]) -> Dict[str, int]:
         counts[word] = old_value + 1
     return counts
 
+
 def get_top_words(word_counts: Dict[str, int], top: int = 25) -> Dict[str, int]:
     """
     Sorts the word_counts dictionary in descending order by value, and returns top items
     """
     top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:top]
-    return {
-        word: count
-        for word, count in top_words
-    }
+    return {word: count for word, count in top_words}
+
 
 def get_stopwords(stopword_path: str) -> List[str]:
     """
@@ -50,7 +63,10 @@ def get_stopwords(stopword_path: str) -> List[str]:
     file_content = read_file(stopword_path)
     return file_content.split()
 
-def remove_words_from_count(word_count: Dict[str, int], to_remove: List[str]) -> Dict[str, int]:
+
+def remove_words_from_count(
+    word_count: Dict[str, int], to_remove: List[str]
+) -> Dict[str, int]:
     """
     Removes all strings in to_remove from the word_count
     """
@@ -59,34 +75,91 @@ def remove_words_from_count(word_count: Dict[str, int], to_remove: List[str]) ->
         copy_count.pop(removal, None)
     return copy_count
 
+
 def normalize_word_tense(words: List[str]) -> List[str]:
     """
     Normalizes word tense by replacing the ends of inflected words with their original suffix
     """
     normalized_words = []
     for word in words:
-        replaced=False
+        replaced = False
         for inflection, replacement in ORDERED_INFLECTION_REPLACEMENTS:
             if word.endswith(inflection):
                 new_word = word.rstrip(inflection) + replacement
                 normalized_words.append(new_word)
                 replaced = True
                 break
-        
+
         if not replaced:
             normalized_words.append(word)
-    
+
     return normalized_words
+
+
+def save_results(
+    results: Dict[str, int],
+    source_path: str,
+    stopword_path: str,
+    normalized_words: bool,
+    results_path: str,
+):
+    old_results = []
+    if os.path.isfile(results_path):
+        with open(results_path, "r") as results_file:
+            results_contents = results_file.read()
+            if results_contents:
+                old_results = json.loads(results_contents)
+
+    results_record = {
+        "id": str(uuid.uuid4()),
+        "filepath": source_path,
+        "stopword-path": stopword_path,
+        "word-stems": normalized_words,
+        "results": results,
+        "timestamp": datetime.now().isoformat(),
+    }
+    new_results = [results_record, *old_results[:9]]
+    with open(results_path, "w") as results_file:
+        json.dump(new_results, results_file, indent=2)
+
+
+def print_results(results_path):
+    if not os.path.isfile(results_path):
+        print("No results exist")
+        return
+
+    with open(results_path, "r") as results_file:
+        results_contents = results_file.read()
+        if results_contents:
+            pprint(json.loads(results_contents))
+        else:
+            print("No results exist")
+
 
 @click.group()
 def cli():
     pass
 
+
 @click.command()
 @click.option("--filepath", help="Filepath to count words from", required=True)
 @click.option("--stopword-path", help="Filepath containing list of stopwords")
-@click.option("--word-stems", default=False, help="Whether or not different inflections count as the same word")
-def word_count(filepath: str, stopword_path: Optional[str], word_stems: bool = False):
+@click.option(
+    "--word-stems",
+    default=False,
+    help="Whether or not different inflections count as the same word",
+)
+@click.option("--save", default=True, help="Enable saving results for later review")
+@click.option(
+    "--results-path", default="out/results.json", help="Path to the results file"
+)
+def word_count(
+    filepath: str,
+    stopword_path: Optional[str],
+    word_stems: bool,
+    save: bool,
+    results_path: str,
+):
     stopwords = []
     if stopword_path:
         stopwords = get_stopwords(stopword_path)
@@ -99,6 +172,19 @@ def word_count(filepath: str, stopword_path: Optional[str], word_stems: bool = F
     top_words = get_top_words(word_counts)
     pprint(top_words, sort_dicts=False)
 
+    if save:
+        save_results(top_words, filepath, stopword_path, word_stems, results_path)
+
+
+@click.command()
+@click.option(
+    "--results-path", default="out/results.json", help="Path to the results file"
+)
+def review(results_path: str):
+    print_results(results_path)
+
+
 if __name__ == "__main__":
     cli.add_command(word_count)
+    cli.add_command(review)
     cli()
